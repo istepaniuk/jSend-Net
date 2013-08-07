14e55aa8574e3dd4d6647f97ef71d438ff89d30e
Iván Stepaniuk <istepaniuk@gmail.com>
2013-08-08 00:03:12 +0200

    Added test project

---
+++ JSendNetTests/DeJSendTests.cs
@@ -0,0 +1,15 @@
+using System;
+using NUnit.Framework;
+
+namespace IStepaniuk.JSendNetTests
+{
+    [TestFixture()]
+    public class DeJSendTests
+    {
+        [Test()]
+        public void TestCase ()
+        {
+        }
+    }
+}
+
