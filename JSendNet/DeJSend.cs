248c6767e00dbd1fd3c90b8c404e456f86e9376b
Iván Stepaniuk <istepaniuk@gmail.com>
2013-08-08 01:19:22 +0200

    renaming variables and getting rid of hungarian notation, it's 2013\!

--- JSendNet/DeJSend.cs
+++ JSendNet/DeJSend.cs
@@ -3,52 +3,53 @@ using System.Collections.Generic;
 
 namespace IStepaniuk.JSendNet
 {
-	public class DeJSend
-	{
-        public string GetData (string iData)
-		{
-			if (String.IsNullOrEmpty(iData)) {
-				return "";			
-			}
-            var elements = iData.Split (new string[] { "==" }, StringSplitOptions.None);
-            var s1 = elements[0];
-            var s2 = elements.Length > 1 ? elements[1] : null;
-            var sDataTmp1 = DecompressLZW(DecodeBinary(Decode847(s1)));
+    public class DeJSend
+    {
+        public string GetData (string input)
+        {
+            if (String.IsNullOrEmpty (input)) {
+                return "";			
+            }
+            var elements = input.Split (new string[] { "==" }, StringSplitOptions.None);
+            var s1 = elements [0];
+            var s2 = elements.Length > 1 ? elements [1] : null;
+            var sDataTmp1 = DecompressLZW (DecodeBinary(Decode847(s1)));
             string sDataTmp2 = null;
-			if (!String.IsNullOrEmpty(s2))
-				sDataTmp2 = DecompressLZW (DecodeBinary (Decode847 (s2)));
-			String sData = "";
+            if (!String.IsNullOrEmpty (s2))
+                sDataTmp2 = DecompressLZW (DecodeBinary (Decode847 (s2)));
+            String sData = "";
 			
 			
-			if (!String.IsNullOrEmpty (sDataTmp2)) {
-				for (Int32 i = 0; i < sDataTmp1.Length; i++) {
-					Char sTmp1 = sDataTmp1[i];
-					Int32 sTmp2 = (Int32)sDataTmp2[i];
-					if (sTmp2 != 224)
-						sData += (Char) ((Int32)sTmp1 + 256 * sTmp2); 
-					else if ((Int32)sTmp1 > 127)
-						sData += sTmp1.ToString();
-					else
-						sData += sTmp1;
-				}
-			} else {
-				sData = sDataTmp1;
-			}
+            if (!String.IsNullOrEmpty (sDataTmp2)) {
+                for (Int32 i = 0; i < sDataTmp1.Length; i++) {
+                    Char sTmp1 = sDataTmp1 [i];
+                    Int32 sTmp2 = (Int32)sDataTmp2 [i];
+                    if (sTmp2 != 224)
+                        sData += (Char)((Int32)sTmp1 + 256 * sTmp2);
+                    else if ((Int32)sTmp1 > 127)
+                        sData += sTmp1.ToString ();
+                    else
+                        sData += sTmp1;
+                }
+            } else {
+                sData = sDataTmp1;
+            }
 			
-			foreach (KeyValuePair<Byte, Char> pair in LookUpTable()) {
-				String strFrom = "" ;
-				strFrom += (Char)pair.Key;
+            foreach (KeyValuePair<Byte, Char> pair in LookUpTable()) {
+                String strFrom = "";
+                strFrom += (Char)pair.Key;
 				
-				String strTo =  ""; 
-				strTo += pair.Value;
+                String strTo = ""; 
+                strTo += pair.Value;
 				
-				sData = sData.Replace (strFrom, strTo);
-			}
-			return sData.Substring (1);
+                sData = sData.Replace (strFrom, strTo);
+            }
+            return sData.Substring (1);
 			
-		}
+        }
 
-        private Dictionary<Byte, Char> LookUpTable(){
+        private Dictionary<Byte, Char> LookUpTable ()
+        {
             return new Dictionary<Byte, Char> {
                 { 128, (Char) 8364},
                 { 130, (Char) 8218},
@@ -80,99 +81,99 @@ namespace IStepaniuk.JSendNet
             };
         }
 
-        private List<Char> Decode847 (string sChars)
-		{
+        private List<Char> Decode847 (string input)
+        {
             var iByte = 7;
             var iMask = 0;
             var aCharCodes = new List<Char> ();
 			
-            for (var i = 0; i < sChars.Length; i++) {
-                var iValue = (Int32)(sChars[i]);
+            for (var i = 0; i < input.Length; i++) {
+                var iValue = (Int32)(input [i]);
 				
-				if (iValue == 61) {
-					i++;
-					iValue = (Int32)(sChars[i]) - 16;
-				}
+                if (iValue == 61) {
+                    i++;
+                    iValue = (Int32)(input [i]) - 16;
+                }
 				
-				if (iByte > 6) {
-					iMask = iValue;
-					iByte = 0;
+                if (iByte > 6) {
+                    iMask = iValue;
+                    iByte = 0;
 					
-				} else {
-					Int32 pt = (Int32)Math.Ceiling (Math.Pow (2, iByte));
-					if ((iMask & pt) == pt)
-						iValue += 128;
-					aCharCodes.Add ((Char)iValue);
-					iByte++;
-				}
-			}
-			return aCharCodes;
+                } else {
+                    Int32 pt = (Int32)Math.Ceiling (Math.Pow (2, iByte));
+                    if ((iMask & pt) == pt)
+                        iValue += 128;
+                    aCharCodes.Add ((Char)iValue);
+                    iByte++;
+                }
+            }
+            return aCharCodes;
 			
-		}
+        }
 
-		private List<Char> DecodeBinary (List<Char> aCharCodes)
-		{
+        private List<Char> DecodeBinary (List<Char> input)
+        {
             var aCodes = new List<Char> ();
             var iDictCount = 256;
             var iBits = 8;
             var iRest = 0;
             var iRestLength = 0;
 			
-			for (Int32 i = 0; i < aCharCodes.Count; i++) {
-				iRest = (iRest << 8) + (Int32)aCharCodes[i];
-				iRestLength += 8;
+            for (Int32 i = 0; i < input.Count; i++) {
+                iRest = (iRest << 8) + (Int32)input [i];
+                iRestLength += 8;
 				
-				if (iRestLength >= iBits) {
-					iRestLength -= iBits;
-					aCodes.Add ((Char)(iRest >> iRestLength));
-					iRest &= (1 << iRestLength) - 1;
-					iDictCount++;
-					if ((iDictCount >> iBits) > 0)
-						iBits++;
-				}
-			}
-			return aCodes;
+                if (iRestLength >= iBits) {
+                    iRestLength -= iBits;
+                    aCodes.Add ((Char)(iRest >> iRestLength));
+                    iRest &= (1 << iRestLength) - 1;
+                    iDictCount++;
+                    if ((iDictCount >> iBits) > 0)
+                        iBits++;
+                }
+            }
+            return aCodes;
 			
-		}
+        }
 
-		private string DecompressLZW (List<Char> aCodes)
-		{
-            var sData = "";
-            var oDictionary = new List<String> ();
+        private string DecompressLZW (List<Char> input)
+        {
+            var data = "";
+            var dictionary = new List<String> ();
 			
-			for (Int32 i = 0; i < 256; i++) {
-                var ch = (Char) i;
-				oDictionary.Add (ch.ToString ());
-			}
+            for (Int32 i = 0; i < 256; i++) {
+                var ch = (Char)i;
+                dictionary.Add (ch.ToString ());
+            }
             var sWord = "";
 			
-            for (var sKey = 0; sKey < aCodes.Count; sKey++) {
+            for (var sKey = 0; sKey < input.Count; sKey++) {
                 var sElement = "";
-                var iCode = aCodes[sKey];
+                var iCode = input [sKey];
 				
-				if (!(iCode >= 0 && iCode < oDictionary.Count)) {
+                if (!(iCode >= 0 && iCode < dictionary.Count)) {
 					
-					if (sWord.Length > 0) {
-						sElement = sWord + sWord[0];
+                    if (sWord.Length > 0) {
+                        sElement = sWord + sWord [0];
 						
-					} else {
-						sElement = "";
-					}
+                    } else {
+                        sElement = "";
+                    }
 					
-				} else {
-					sElement = oDictionary[iCode];
-				}
-				sData += sElement;
-				if (sKey > 0) {
+                } else {
+                    sElement = dictionary [iCode];
+                }
+                data += sElement;
+                if (sKey > 0) {
                     var newEntry = "";
-					if (sElement.Length > 0) {
-						newEntry += sWord + sElement[0];
-					}
-					oDictionary.Add (newEntry);
-				}
-				sWord = sElement;
-			}
-			return sData;
-		}
-	}
+                    if (sElement.Length > 0) {
+                        newEntry += sWord + sElement [0];
+                    }
+                    dictionary.Add (newEntry);
+                }
+                sWord = sElement;
+            }
+            return data;
+        }
+    }
 }
